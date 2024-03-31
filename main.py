3f2072febc346921ebb901940f4c930a6d82aeec
Meidozuki <68063235+Meidozuki@users.noreply.github.com>
2024-03-31 19:36:54 +0800

    further decoupling by TableItemChecklist

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import os, sys
 from typing import *
 
 import numpy as np
-from PIL import Image, ImageQt
 from abc import ABC
 
 from PySide6.QtCore import QSize, Qt, Slot, QFileInfo
@@ -17,11 +16,6 @@ from PySide6.QtGui import (
 from src import MainWindow, vbao
 from src import table_item, open_button, menu_bar, model
 
-class A:
-    @classmethod
-    def f1(cls):
-        print(1)
-    f2=f1
 
 if __name__ == '__main__':
     vbao.use_easydict()
